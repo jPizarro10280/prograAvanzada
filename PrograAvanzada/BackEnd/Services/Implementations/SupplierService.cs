987caac5e6af87c98605f85651650f72fc650f02
jPizarro10280 <jpizarro10280@ufide.ac.cr>
2025-01-28 15:31:59 -0600

    se agrego services y interface en backEnd

---
+++ PrograAvanzada/BackEnd/Services/Implementations/SupplierService.cs
@@ -0,0 +1,35 @@
+﻿using BackEnd.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace BackEnd.Services.Implementations
+{
+    public class SupplierService : ISupplierService
+    {
+        private ISupplierDAL _supplierDAL;
+
+        public SupplierService(ISupplierDAL supplierDAL)
+        {
+            _supplierDAL = supplierDAL;
+        }
+        public void AddSupplier(Supplier supplier)
+        {
+            _supplierDAL.AddSupplier(supplier);
+        }
+
+        public void DeleteSupplier(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Supplier> GetSuppliers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSupplier(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
