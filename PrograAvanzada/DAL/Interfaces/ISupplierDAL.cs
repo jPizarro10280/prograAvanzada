bb91631b22b3491e635302846a2df63709bd8ff8
jPizarro10280 <jpizarro10280@ufide.ac.cr>
2025-01-28 15:20:52 -0600

    se creo ISupplierDAL y SupplierDAL

---
+++ PrograAvanzada/DAL/Interfaces/ISupplierDAL.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface ISupplierDAL
+    {
+        bool AddSupplier(Supplier supplier);
+        bool UpdateSupplier(Supplier supplier);
+        bool DeleteSupplier(int id);
+        List<Supplier> GetSuppliers();
+
+    }
+}
---
+++ PrograAvanzada/DAL/Interfaces/SupplierDAL.cs
@@ -0,0 +1,46 @@
+﻿using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public class SupplierDAL : ISupplierDAL
+    {
+        private NorthwndContext _context;
+        public SupplierDAL(NorthwndContext context)
+        {
+            _context = context;
+        }
+        public bool AddSupplier(Supplier supplier)
+        {
+            try
+            {
+                _context.Add(supplier);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteSupplier(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Supplier> GetSuppliers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateSupplier(Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
